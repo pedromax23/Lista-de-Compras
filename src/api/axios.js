05c35a3f68db0ed530459eba0b96cbeb01ab30ed
pedromax23 <pedromorgade1234@gmail.com>
2024-06-03 17:34:30 -0300

    Change production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API_URL = import.meta.env.VITE_API_URL_LOCAL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const instance = axios.create({
     baseURL: API_URL,
